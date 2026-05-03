f591d5ffe334e27d6df880349f72919ec4ad8066
UrpirioDev <urpiriojunior@gmail.com>
2026-05-03 15:24:36 -0400

    feat: inject PrismaModule into schedules and library modules

--- Homework/backend-homework/src/library/library.module.ts
+++ Homework/backend-homework/src/library/library.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LibraryService } from './library.service';
 import { LibraryController } from './library.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [LibraryService],
   controllers: [LibraryController]
 })
