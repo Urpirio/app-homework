f8e24ec27f03de0d94f173bd1ab180d44be0f634
UrpirioDev <urpiriojunior@gmail.com>
2026-05-07 10:05:25 -0400

    feat: register InstitutionsService and InstitutionsController in institutions module

--- Homework/backend-homework/src/institutions/institutions.module.ts
+++ Homework/backend-homework/src/institutions/institutions.module.ts
@@ -1,4 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
+import { InstitutionsService } from './institutions.service';
+import { InstitutionsController } from './institutions.controller';
 
 @Module({
   imports: [PrismaModule, UsersModule],
